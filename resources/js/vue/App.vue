42195a36d1f3629614fb86b1333375a5aa495a58
Prajan Shrestha <prajan@mavorion.com>
2022-11-10 18:27:38 +0545

    login and register on progress

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,7 +1,15 @@
 <template>
-    <router-link :to="{ name: 'Home' }">Home</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/login">Login</router-link>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark ps-5">
+        <router-link class="navbar-brand" :to="{ name: 'Home' }">Home</router-link>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <router-link class="text-white text-decoration-none me-3" to="/register">Register</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="text-white text-decoration-none" to="/login">Login</router-link>
+            </li>
+        </ul>
+    </nav>
 
     <router-view></router-view>
 </template>
--- resources/js/vue/pages/Login.vue
+++ resources/js/vue/pages/Login.vue
@@ -1,10 +1,45 @@
 <template>
-  <h1>Login</h1>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6 mt-4">
+            <form @submit.prevent="login">
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" id="email" class="form-control" v-model="form.email">
+                </div> <br>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" class="form-control" v-model="form.password">
+                </div> <br>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            form: {
+                email: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        login() {
+            let that = this;
+            axios
+                .post("/api/login", {
+                    form: that.form
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+        }
+    }
 }
 </script>
 
