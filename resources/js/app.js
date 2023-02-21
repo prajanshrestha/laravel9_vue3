5ef83dd3210a2a8906f794215f1d888fb719078d
Prajan Shrestha <prajan@mavorion.com>
2023-02-21 12:10:10 +0545

    vuex learn

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,9 @@ import {createApp} from 'vue'
 import App from './vue/App.vue'
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.css';
-// import { store } from "./store/store";
+import store from "./store/store";
 
 createApp(App)
     .use(router)
-    // .use(store)
+    .use(store)
     .mount("#app")
--- resources/js/vue/pages/Dashboard.vue
+++ resources/js/vue/pages/Dashboard.vue
@@ -4,24 +4,25 @@
     <button type="button" class="btn btn-dark mt-2" @click="logout">Logout</button>
 
     <hr>
-
-    <product-list-one :products='products'></product-list-one>
-    <product-list-two :products='products'></product-list-two>
+    <div>
+        <product-list-one></product-list-one>
+    </div>
+    <div class="mt-5">
+        <product-list-two></product-list-two>
+    </div>
   </div>
 </template>
 
 <script>
 import ProductListOne from './ProductListOne.vue';
-import ProductLisTwo from './ProductListTwo.vue';
+import ProductListTwo from './ProductListTwo.vue';
 export default {
+    components: {
+        ProductListOne, ProductListTwo
+    },
     data() {
         return {
-            products: [
-                { name: 'Banana Skin', price: 20 },
-                { name: 'Shiny Star', price: 40 },
-                { name: 'Green Shells', price: 60 },
-                { name: 'Red Shells', price: 80 },
-            ]
+
         }
     },
     methods: {
